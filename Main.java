09f13714b4c247478c97ecfba1f85cf73ba0c344
Katerina <katerinaplatonova63@gmail.com>
2018-07-30 21:21:22 +0400

    корректировка исходной строки добавление методов split() replace() объединение блоков try{}

--- Main.java
+++ Main.java
@@ -1,48 +1,46 @@
+
 public class Main {
 
-    static String str = ("1312232256713310");
+    static String str = ("1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0");
     static String[][] arrayString = new String[4][4];
     static int[][] arrayInt = new int[4][4];
 
     public static void main(String[] args) {
-
-        System.out.println("---Массив строк---");
         try {
+            System.out.println("---Массив строк---");
             fromStringToArray(str);
+
+            System.out.println("---Массив чисел---");
+            makeInt(arrayString);
+        } catch (NumberFormatException e) {// если в изначальном строковом массиве было не числовое значеие (символ или слово)
+            System.out.println("Попытка добавить не числовое значение.");
+            e.printStackTrace();
         } catch (IndexOutOfBoundsException e) {
             System.out.println("В массиве не достаточно места.");
             e.printStackTrace();
         }
 
-        System.out.println("---Массив чисел---");
-        try {
-            makeInt(arrayString);
-        } catch (ArrayStoreException e) { // если в изначальном строковом массиве было не числовое значеие (символ или слово)
-            System.out.println("Попытка добавить не числовое значение.");
-            e.printStackTrace();
-        }
-
         System.out.println("---Сумма элементов массива / 2---");
         System.out.println("Результат вычисления = " + countSumAndDivideTwo(arrayInt));
-
     }
 
     static void fromStringToArray(String str) throws IndexOutOfBoundsException { // создание двумерного массива из строки
-        int temp = 0;
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j < str.length(); j++) {
-                String x = String.valueOf(str.charAt(temp)); //??
-                arrayString[i][j] = x;
+        String[] st = str.split("\n");
+        for (int i = 0; i < st.length; i++) {
+            st[i] = st[i].replace(" ", "");
+        }
+        for (int i = 0; i < arrayString.length; i++) {
+            for (int j = 0; j < arrayString.length; j++) {
+                arrayString[i][j] = String.valueOf(st[i].charAt(j));
                 System.out.print(arrayString[i][j] + " ");
-                temp++;
             }
             System.out.println(" ");
         }
     }
 
-    static void makeInt(String[][] array) throws ArrayStoreException { //приведение массива строк к массиву чисел
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+    static void makeInt(String[][] array) throws NumberFormatException { //приведение массива строк к массиву чисел
+        for (int i = 0; i < array[i].length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 int b = Integer.valueOf(array[i][j]);
                 arrayInt[i][j] = b;
                 System.out.print(arrayInt[i][j] + " ");
